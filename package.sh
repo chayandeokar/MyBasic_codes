b3213209e592a3646780d7433f4c4e7d3460ff83
Chayan Deokar <74093567+chayandeokar@users.noreply.github.com>
2023-08-10 15:58:57 +0530

    Create package.sh

---
+++ package.sh
@@ -0,0 +1,15 @@
+if [ MYUID -eq 1010170000 ]
+then
+        echo "we can do"
+        read -p "Enter package name : " pack
+        if rpm -q $pack $> /dev/null
+        then
+                echo "$pack already installed "
+        elif yum install $pack -y &> /dev/null
+        then
+                echo "$pack installed success"
+        else
+                echo "$pack not innstalled"
+        fi
+else
+        echo "worng id"
